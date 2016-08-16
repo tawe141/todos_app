04c1326fd0bc741746d85fbb83a0f3096fe7b063
Eric <erictaw2018@u.northwestern.edu>
2016-08-15 22:40:13 -0500

    add body-parser, requires body testing and AJAX calls from frontend

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 var express = require('express')
-var database = require('database.json')
+// import database file
+var database = require('./database.json')
+// used to parse body from requests
+var bodyParser = require('body-parser')
 var app = express()
+
+// parse JSON data being sent in
+app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
     res.sendFile('base.html')
 })
 
+// GET request: return data from database in JSON format
 app.get('/tasks', function(req, res) {
     res.json(database)
 })
 
+// POST request: append new data onto database, then overwrite old database
 app.post('/tasks', function(req, res) {
-    
+    // database[(Object.keys(database).length + 1).toString()] =
+    console.log(req.body);
 })
 
 app.listen(3000, function() {
