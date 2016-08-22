dd523fdde9447a067b291ebda076ac3f5cdf491d
Eric <erictaw2018@u.northwestern.edu>
2016-08-22 14:26:01 -0500

    task form submission successful

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var express = require('express')
 var database = require('./database.json')
 // used to parse body from requests
 var bodyParser = require('body-parser')
+// easier directory path manipulation
+var path = require('path')
 var app = express()
 
 // parse JSON data being sent in
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function(req, res) {
-    res.sendFile('base.html')
+    res.sendFile(path.join(__dirname, 'base.html'))
 })
 
 // GET request: return data from database in JSON format
